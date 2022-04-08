b975c12899e296bba0bfd02a7121d523521fb94f
AB <ab38000111@gmail.com>
2022-04-08 11:03:20 +0200

    premier commit

---
+++ contact.php
@@ -0,0 +1,39 @@
+<div class="text-center" id="bg-bloc9">
+    <h1 class="pt-5 h2-titre mb-3">CONTACT</h1>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-9 col-sm-12 mx-auto" >
+                <h3 class="h5-titre mb-5">Merci de me contacter pour plus d'informations</h3>
+                <form action="" method="post" class="text-lg-start">
+                    <form>
+                        <div class="mb-4">
+                            <label for="nom" class="form-label">NOM</label>
+                            <input type="text" name="nom" class="form-control" id="nom" placeholder="Votre nom" required>
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="prenom" class="form-label">PRENOM</label>
+                            <input type="text" name="prenom" class="form-control" id="prenom" placeholder="Votre prénom" required>
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="email" class="form-label">E-MAIL</label>
+                            <input type="mail" name="email" class="form-control" id="email" placeholder="Votre e-mail" required>
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="nom" class="form-label">MESSAGE</label>
+                            <textarea rows="6" class="form-control" placeholder="Votre message" required></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-outline-info mt-4 mb-5">Envoyer</button>
+                    </form>
+                </form>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <img src="assets/img/form.jpg" alt="" title="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,307 @@
+<?php
+//sources = https://www.mathieu-crevoulin.com/
+?> 
+
+<!doctype html>
+<html lang="fr">
+
+  <head>
+    <title>Portfolio AB</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+
+      <header class="site-navbar site-navbar-target" role="banner">
+
+        <div class="container">
+          <div class="row align-items-center position-relative">
+
+            <div class="col-3 ">
+              <div class="site-logo">
+                <a href="index.php">Portfolio</a>
+              </div>
+            </div>
+
+            <div class="col-9  text-right">
+              
+
+              <span class="d-inline-block d-lg-none"><a href="#" class="text-white site-menu-toggle js-menu-toggle py-5 text-white"><span class="icon-menu h3 text-white"></span></a></span>
+
+
+              <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav ml-auto ">
+                  <li class="active"><a href="index.php" class="nav-link">Accueil</a></li>
+                  <li><a href="#" class="nav-link">Mes Projets</a></li>
+                  <li><a href="#" class="nav-link">Mes Compétences</a></li>
+                  <li><a href="#" class="nav-link">A propos</a></li>
+                  <li><a href="#" class="nav-link">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+
+            
+          </div>
+        </div>
+
+      </header>
+
+    <div class="ftco-blocks-cover-1">
+      <div class="site-section-cover overlay" data-stellar-background-ratio="0.5" style="background-image: url('images/hero_6.jpg')">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-9">
+              <h1 class="mb-5">DEVELOPPEUR WEB &amp; WEB MOBILE</h1><br><br> <br> <br><br> <br> <br>     <!--
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p><br>  
+              <p><a href="#" class="btn btn-primary">En savoir plus</a></p>  -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+<!--
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <div class="img-years">
+              <img src="images/img_4.jpg" alt="Image" class="img-fluid">
+            </div>
+          </div>
+
+          <div class="col-lg-5 ml-auto pl-lg-5 text-center">
+            <h3 class="scissors text-center">Welcome To PORTFOLIO!</h3>
+            <p class="mb-5 lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure nesciunt nemo vel earum maxime neque!</p>
+            <p><a href="#" class="btn btn-primary">Learn More</a></p>
+          </div>
+-->
+        </div>
+      </div>
+    </div>
+
+    
+
+    <div class="site-section bg-light" id="projets">
+      <div class="container">
+        <div class="row justify-content-center  mb-5">
+          <div class="col-md-7 text-center">
+            <h3 class="scissors text-center">MES PROJETS</h3>
+            <p class="mb-5 lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure nesciunt nemo vel earum maxime neque!</p>
+
+            <p class="text-center">
+              <a href="#" class="btn btn-primary custom-prev">Précédent</a>
+              <a href="#" class="btn btn-primary custom-next">Suivant</a>
+            </p>
+          </div>
+
+        </div>
+        <div class="row">
+          <div class="col-12">
+
+            <div class="nonloop-block-13 owl-carousel d-flex">
+              
+              <div class="item-1 h">
+                <img src="images/img_9.jpg" alt="Image" class="img-fluid">
+                <div class="item-1-contents">
+                  <h3>Restaurant JADOO</h3>
+                </div>
+
+              </div>
+
+              <div class="item-1 h">
+                <img src="images/img_10.jpg" alt="Image" class="img-fluid">
+                <div class="item-1-contents">
+                  <h3>Agence de Voyages</h3>
+                </div>
+                
+              </div>
+
+              <div class="item-1 h">
+                <img src="images/img_9.jpg" alt="Image" class="img-fluid">
+                <div class="item-1-contents">
+                  <h3>Javascript</h3>
+                </div>
+                
+              </div>
+
+              <div class="item-1 h">
+                <img src="images/img_10.jpg" alt="Image" class="img-fluid">
+                <div class="item-1-contents">
+                  <h3>Découverte PHP</h3>
+                </div>
+                
+              </div>
+
+              <div class="item-1 h">
+                <img src="images/img_11.jpg" alt="Image" class="img-fluid">
+                <div class="item-1-contents">
+                  <h3>Thème Wordpress</h3>
+                </div>
+                
+              </div>
+
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="site-section section-3" data-stellar-background-ratio="0.5" style="background-image: url('images/img_9.jpg');">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-9">
+              <h1 class="mb-3 text-white">MES COMPETENCES</h1>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="site-section">
+        <div class="container">
+
+          <div class="col-md-4 mx-auto" id="competences-center">
+                <div class="list-group text-center">
+                    <p class="mx-2 pt-1 h5-titre bloc4-langage">HTML5 - CSS3</p>
+                    <p class="mx-2 pt-3 h5-titre bloc4-langage2">PHP - MYSQL</p>
+                    <p class="mx-2 pt-3 h5-titre bloc4-langage4">JAVASCRIPT</p>
+                    <p class="mx-2 pt-3 pb-3 h5-titre bloc4-langage5">SASS</p>
+                </div>
+          </div>
+
+          <div class="row hair-style">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-12 mx-auto my-5">
+              <a href="#" class="place">
+                <img src="images/hero_5.jpg" alt="Image placeholder">
+                <h2></h2>
+                <span></span>
+              </a>
+            </div>
+          </div>
+
+          <a href="assets/pdf/Projet_4_Portfolio.pdf" class="btn btn-info text-center">
+            <i class="icone-wordpress bi bi-wordpress"></i>
+            Télécharger le CV FACILE
+          </a>
+
+      </div>
+    </div>
+    <!-- END section -->
+
+    <div class="site-section section-3" data-stellar-background-ratio="0.5" style="background-image: url('images/hero_6.jpg');">
+      <div class="container">
+        <div class="row justify-content-center text-center">
+          <div class="col-7 text-center mb-5">
+            <h2 class="text-white scissors primary-color-icon text-center">A PROPOS</h2>
+            <p class="lead text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam quo doloribus, suscipit libero, voluptate aliquam.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid" id="bloc10">
+        <?php
+        require_once "contact.php";
+        ?>
+    </div>
+
+    
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <img src="images/img_8.jpg" alt="Image" class="img-fluid">
+        <!--
+            <h2 class="footer-heading mb-3">A propos</h2>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+        -->
+          </div>
+          <div class="col-lg-8 ml-auto mt-5">
+            <div class="row">
+              <div class="col-lg-6 ml-auto">
+                <ul class="list-unstyled">
+                  <li><a href="#">A propos</a></li>
+                  <li><a href="#">Mentions légales</a></li>
+                  <li><a href="#">Confidentialité - Vie privée</a></li>
+                  <li><a href="#">Cookies</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <h2 class="footer-heading mb-4">Contact</h2>
+                <form action="#" class="d-flex" class="subscribe">
+                  <input type="text" class="form-control mr-3" placeholder="Email">
+                  <input type="submit" value="Valider" class="btn btn-primary">
+                </form>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <div class="border-top pt-5">
+              <p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+  </body>
+
+</html>
+
